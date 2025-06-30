bd554a148bea55e8f94429fdd2438fa542a329a7
Do-Or-D1E <hassanibrar46@gmail.com>
2025-07-01 00:33:51 +0500

    fixed prices not displaying on cabincards

--- components/CabinCard.js
+++ components/CabinCard.js
@@ -1,8 +1,9 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import Link from "next/link";
 
 function CabinCard({ cabin }) {
-  const { id, name, maxcapacity, regularprice, discount, image } = cabin;
+  const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
     <div className="flex border-primary-800 border">
@@ -23,7 +24,7 @@ function CabinCard({ cabin }) {
           <div className="flex gap-3 items-center mb-2">
             <UsersIcon className="h-5 w-5 text-primary-600" />
             <p className="text-lg text-primary-200">
-              For up to <span className="font-bold">{maxcapacity}</span> guests
+              For up to <span className="font-bold">{maxCapacity}</span> guests
             </p>
           </div>
 
@@ -31,26 +32,26 @@ function CabinCard({ cabin }) {
             {discount > 0 ? (
               <>
                 <span className="text-3xl font-[350]">
-                  ${regularprice - discount}
+                  ${regularPrice - discount}
                 </span>
                 <span className="line-through font-semibold text-primary-600">
-                  ${regularprice}
+                  ${regularPrice}
                 </span>
               </>
             ) : (
-              <span className="text-3xl font-[350]">${regularprice}</span>
+              <span className="text-3xl font-[350]">${regularPrice}</span>
             )}
             <span className="text-primary-200">/ night</span>
           </p>
         </div>
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+          <Link
             href={`/cabins/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
